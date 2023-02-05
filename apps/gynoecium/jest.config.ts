8709f9613394e13d2a29e59586b80ad36c1a6538
Yury Zotov <batzotov@gmail.com>
2023-02-06 00:11:59 +0300

    chore: init backend

---
+++ apps/gynoecium/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'gynoecium',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/gynoecium',
+};
