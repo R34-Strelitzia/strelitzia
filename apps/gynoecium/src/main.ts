25a61c6adbf9207435d02b7c962727e874098b82
Yury Zotov <batzotov@gmail.com>
2023-02-11 10:26:04 +0300

    chore: add prisma shutdown hook

--- apps/gynoecium/src/main.ts
+++ apps/gynoecium/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { PrismaService } from 'nestjs-prisma';
 
 import { AppModule } from './app/app.module';
 
@@ -12,6 +13,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  /*
+   * Check this: https://docs.nestjs.com/recipes/prisma#issues-with-enableshutdownhooks
+   */
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
