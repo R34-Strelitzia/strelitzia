6e3b3a61536009a0f2c5567ecce4d87a81c78c87
Yury Zotov <batzotov@gmail.com>
2023-02-11 11:52:14 +0300

    chore: change global api prefix for versioning

--- apps/gynoecium/src/main.ts
+++ apps/gynoecium/src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = 'api/v2';
   app.setGlobalPrefix(globalPrefix);
 
   /*
