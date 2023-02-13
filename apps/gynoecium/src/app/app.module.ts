c4881409243426a67c20f500874f15c43d7bdc7b
Yury Zotov <batzotov@gmail.com>
2023-02-13 19:16:48 +0300

    chore: add users module

--- apps/gynoecium/src/app/app.module.ts
+++ apps/gynoecium/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '@strelitzia/prisma';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, PrismaModule],
 })
 export class AppModule {}
