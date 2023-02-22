a6e44ec3e81ddf82d1771bde70973f08c6e49225
Yury Zotov <batzotov@gmail.com>
2023-02-22 18:01:51 +0300

    fix: missing auth exports

--- libs/auth/src/index.ts
+++ libs/auth/src/index.ts
@@ -1,1 +1,9 @@
 export * from './lib/auth.module';
+export * from './lib/decorators';
+export type {
+  IAuthenticatedUser,
+  IJwtPayload,
+  IJwtRefreshPayload,
+} from './lib/interfaces';
+export * from './lib/guards';
+export type { AuthService } from './lib/auth.service';
